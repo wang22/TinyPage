9736e1465562d15889f35e61be2d50ed67e7a6e4
小王王 <pause22@163.com>
2020-11-14 22:35:25 +0800

    add center content

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,6 +4,9 @@
     <Navbar />
   </div>
   <div class="ml-64 bg-gray-100 min-h-screen">
+    <div class="xl:w-3/4 lg:w-full md:w-full sm:w-full mx-auto px-5 h-screen min-h-screen">
+      this is center content
+    </div>
   </div>
 </div>
 </template>
