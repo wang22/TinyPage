31ff4ac3b3bcdf1fd38a0c8d7332153cf525b5a8
DESKTOP-7RAI0UV\GuoAnYinZhang <pause22@163.com>
2020-11-17 15:09:25 +0800

    add router

---
+++ src/layout/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div
+      class="w-64 bg-white fixed inset-y-0 left-0 border-gray-400 shadow-md border-r-"
+    >
+      <t-navbar />
+    </div>
+    <div class="ml-64 bg-gray-100 min-h-screen pb-20">
+      <div
+        class="xl:w-3/4 lg:w-full md:w-full sm:w-full mx-auto px-5 min-h-screen"
+      >
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
