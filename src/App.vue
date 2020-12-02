fc3f4b68fb9dbd1bc755f6e596b44e87f9d5637c
DESKTOP-7RAI0UV\GuoAnYinZhang <pause22@163.com>
2020-12-02 10:40:23 +0800

    new

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="divide-y divide-gray-100">
+    <Nav>
+      <NavItem href="/featured" isActive>Featured</NavItem>
+      <NavItem href="/popular">Popular</NavItem>
+      <NavItem href="/recent">Recent</NavItem>
+    </Nav>
+    <List>
+      <ListItem v-for="recipe in recipes" :key="recipe.id" :recipe="recipe" />
+    </List>
+  </div>
+</template>
+
+<script>
+import Nav from './components/Nav/Nav.vue'
+import NavItem from './components/Nav/NavItem.vue'
+import List from './components/Nav/List.vue'
+import ListItem from './components/Nav/ListItem.vue'
+
+export default {
+  props: ['recipes'],
+  components: {
+    Nav,
+    NavItem,
+    List,
+    ListItem,
+  },
+}
+</script>
