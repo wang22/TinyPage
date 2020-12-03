fed5d2a82129c6f7df4052e2dc1e0452dd321d38
DESKTOP-7RAI0UV\GuoAnYinZhang <pause22@163.com>
2020-12-03 18:27:41 +0800

    add vue tailwind

---
+++ src/layout/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <div class="fixed w-64 bg-gray-100 min-h-screen">
+            sdfasdf
+        </div>
+
+        <div class="ml-64 bg-blue-100">
+            dddd
+        </div>
+    </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
+const { colors } = require('tailwindcss/defaultTheme')
 module.exports = {
   future: {
   },
   purge: [],
   theme: {
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      gray: {
+        '100': '#f5f5f5',
+        '200': '#eeeeee',
+        '300': '#e0e0e0',
+        '400': '#bdbdbd',
+        '500': '#9e9e9e',
+        '600': '#757575',
+        '700': '#616161',
+        '800': '#424242',
+        '900': '#212121',
+      },
+      red: colors.red,
+      yellow: colors.yellow,
+      green: colors.green,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.purple,
+    },
     extend: {},
   },
   variants: {},
