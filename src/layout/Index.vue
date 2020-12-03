4054c6e9dce43cccfca623d0625a26d6cd4cc9a5
小王王 <pause22@163.com>
2020-12-03 23:40:40 +0800

    add nav

--- src/layout/Index.vue
+++ src/layout/Index.vue
@@ -17,8 +17,9 @@
           <div class="self-center">&lt;&gt;</div>
         </div>
         <div class="px-3 mt-8">
-          <t-input placeholder="search" class="w-full shadow" />
-          <ul class="mt-8 font-bold">
+          <t-input placeholder="search" class="w-full shadow-sm" />
+          <label class="mt-8 block uppercase text-sm text-gray-500">manage</label>
+          <ul class="font-bold mt-2">
             <li>
               <a class="p-3 rounded-md block hover:bg-gray-300" href="hello"
                 >Posts</a
@@ -37,6 +38,24 @@
             <li>
               <a class="p-3 rounded-md block hover:bg-gray-300" href="hello"
                 >Pages</a
+              >
+            </li>
+          </ul>
+          <label class="mt-4 block uppercase text-sm text-gray-500">settings</label>
+          <ul class="font-bold mt-2">
+            <li>
+              <a class="p-3 rounded-md block hover:bg-gray-300" href="hello"
+                >General</a
+              >
+            </li>
+            <li>
+              <a class="p-3 rounded-md block hover:bg-gray-300" href="hello"
+                >Design</a
+              >
+            </li>
+            <li>
+              <a class="p-3 rounded-md block hover:bg-gray-300" href="hello"
+                >Code injection</a
               >
             </li>
           </ul>
