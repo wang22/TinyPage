b8eae96486c3f5406af2ccf3f1af29a43a94ba72
小王王 <pause22@163.com>
2020-12-13 23:22:42 +0800

    明天继续

--- src/views/post/modal/AddChannel.vue
+++ src/views/post/modal/AddChannel.vue
@@ -22,17 +22,8 @@
       <template #right>
         <div style="width: 200px">
           <b-badge>Public</b-badge>
-          <b-form-input type="range" min="0" max="3"></b-form-input>
+          <b-form-input type="range" @change="onVisiableChange" min="0" max="3"></b-form-input>
         </div>
-      </template>
-      <template #collapse>
-        <b-form-group
-          id="input-group-1"
-          label="Member visiable:"
-          label-for="input-1"
-        >
-          <b-form-input id="range-1" v-model="value" type="range" min="0" max="5"></b-form-input>
-        </b-form-group>
       </template>
     </t-accordion>
     <t-accordion title="SEO" mark="options" :icon="icons.seo">
@@ -102,15 +93,6 @@ export default {
         visiable: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" /></svg>',
         field: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17V7m0 10a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h2a2 2 0 012 2m0 10a2 2 0 002 2h2a2 2 0 002-2M9 7a2 2 0 012-2h2a2 2 0 012 2m0 10V7m0 10a2 2 0 002 2h2a2 2 0 002-2V7a2 2 0 00-2-2h-2a2 2 0 00-2 2" /></svg>'
       },
-      visiableSelected: ['public', 'member', 'paid', 'private'],
-      switches: {
-        memberVisiable: [
-          { text: 'Public', value: 'public' },
-          { text: 'Member only', value: 'member', disabled: false },
-          { text: 'Paid member only', value: 'paid', disabled: false },
-          { text: 'Private', value: 'private', disabled: false }
-        ]
-      },
       channel: {
         name: '',
         path: ''
@@ -120,6 +102,9 @@ export default {
   methods: {
     channelNameDetail (status) {
       this.$refs.channelPath.collapseStatus(status)
+    },
+    onVisiableChange (val) {
+      console.log(val)
     }
   }
 }
