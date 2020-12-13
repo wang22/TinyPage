393e926ced154dca5e3c39d1fd77093871fca3c7
小王王 <pause22@163.com>
2020-12-13 14:28:07 +0800

    删除多余项

--- src/views/post/modal/AddChannel.vue
+++ src/views/post/modal/AddChannel.vue
@@ -40,33 +40,6 @@
         </b-form>
       </template>
     </t-accordion>
-    <t-accordion title="SEO" mark="options" :icon="icons.seo">
-      <template #collapse>
-        <b-form>
-          <b-form-group
-            id="input-group-1"
-            label="Email address:"
-            label-for="input-1"
-            description="We'll never share your email with anyone else."
-          >
-            <b-form-input
-              id="input-1"
-              type="email"
-              required
-              placeholder="Enter email"
-            ></b-form-input>
-          </b-form-group>
-
-          <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-            <b-form-input
-              id="input-2"
-              required
-              placeholder="Enter name"
-            ></b-form-input>
-          </b-form-group>
-        </b-form>
-      </template>
-    </t-accordion>
   </div>
 </template>
 <script>
