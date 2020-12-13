279e61a50ec96ed7463b5994124cd4c445be76a4
小王王 <pause22@163.com>
2020-12-13 22:57:01 +0800

    可见性需要使用滑块，目前没有找到合适的组件

--- src/views/post/modal/AddChannel.vue
+++ src/views/post/modal/AddChannel.vue
@@ -7,9 +7,10 @@
     </t-accordion>
     <t-accordion title="Channel path" mark="channel path" :icon="icons.path" ref="channelPath">
       <template #right>
-        <b-form-input placeholder="Enter channel path" v-model="channel.path" @focus="channelNameDetail(true)"></b-form-input>
+        <b-form-input placeholder="Enter channel path" v-model="channel.path" @focus="channelNameDetail(true)" @blur="channelNameDetail(false)"></b-form-input>
       </template>
       <template #collapse>
+        <h6>Browser Preview</h6>
         <t-v-browser :url="`https://localhost/${channel.path}`" />
       </template>
     </t-accordion>
@@ -48,6 +49,7 @@
           label-for="input-1"
         >
           <b-form-checkbox-group
+            v-model="visiableSelected"
             :options="switches.memberVisiable"
             switches
             stacked
@@ -95,12 +97,13 @@ export default {
         visiable: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" /></svg>',
         field: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17V7m0 10a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h2a2 2 0 012 2m0 10a2 2 0 002 2h2a2 2 0 002-2M9 7a2 2 0 012-2h2a2 2 0 012 2m0 10V7m0 10a2 2 0 002 2h2a2 2 0 002-2V7a2 2 0 00-2-2h-2a2 2 0 00-2 2" /></svg>'
       },
+      visiableSelected: ['public', 'member', 'paid', 'private'],
       switches: {
         memberVisiable: [
-          { text: 'Public', value: 'red' },
-          { text: 'Member only', value: 'green' },
-          { text: 'Paid member only', value: 'yellow' },
-          { text: 'Private', value: 'blue' }
+          { text: 'Public', value: 'public' },
+          { text: 'Member only', value: 'member', disabled: false },
+          { text: 'Paid member only', value: 'paid', disabled: false },
+          { text: 'Private', value: 'private', disabled: false }
         ]
       },
       channel: {
