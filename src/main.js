ea35fcd33a56f2403a772a9d5f2626e129d45dd9
DESKTOP-7RAI0UV\GuoAnYinZhang <pause22@163.com>
2020-12-22 13:44:12 +0800

    add axios

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { VueAxios } from './utils/request'
 import './css/style.scss'
 import components from './components'
 
+Vue.use(VueAxios)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.config.productionTip = false
---
+++ src/utils/request.js
@@ -0,0 +1,67 @@
+import axios from 'axios'
+import { VueAxios } from './axios'
+import storage from 'store'
+
+
+export const ACCESS_TOKEN = 'Access-Token'
+
+const request = axios.create({
+  // API 请求的默认前缀
+  baseURL: 'http://localhost:8080',
+  timeout: 6000 // 请求超时时间
+})
+
+// // 异常拦截处理器
+const errorHandler = (error) => {
+  if (error.response) {
+    // const data = error.response.data
+    // 从 localstorage 获取 token
+    // const token = storage.get(ACCESS_TOKEN)
+    if (error.response.status === 403) {
+      // notification.error({
+      //   message: 'Forbidden',
+      //   description: data.message
+      // })
+    }
+    if (error.response.status === 401) {
+      // notification.error({
+      //   message: 'Unauthorized',
+      //   description: 'Authorization verification failed'
+      // })
+      // if (token) {
+      //   store.dispatch('Logout').then(() => {
+      //     setTimeout(() => {
+      //       window.location.reload()
+      //     }, 1500)
+      //   })
+      // }
+    }
+  }
+  return Promise.reject(error)
+}
+
+request.interceptors.request.use(config => {
+  const token = storage.get(ACCESS_TOKEN)
+  if (token) {
+    config.headers['Authorization'] = token
+  }
+  return config
+}, errorHandler)
+
+request.interceptors.response.use((response) => {
+  return response.data
+}, errorHandler)
+
+const installer = {
+  vm: {},
+  install (Vue) {
+    Vue.use(VueAxios, request)
+  }
+}
+
+export default request
+
+export {
+  installer as VueAxios,
+  request as axios
+}
